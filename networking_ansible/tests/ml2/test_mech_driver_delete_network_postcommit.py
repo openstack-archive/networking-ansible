e0567d85144ab31e1e354be8e1bd1ed6de83e711
Dan Radez <dradez@redhat.com>
2018-05-10 15:09:56 -0400

    Initial Unit tests
    Change-Id: I3941b63143445b6a4cbd53fac8329fafa5438f14

---
+++ networking_ansible/tests/ml2/test_mech_driver_delete_network_postcommit.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2018 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from mock import patch
+
+from networking_ansible.tests.base import NetworkingAnsibleTestCase
+
+
+# Cannot call with autospec=True, the resulting mock object doesn't
+# include the function assert_called_once_with
+@patch('networking_ansible.ansible_networking.'
+       'AnsibleNetworking.delete_network')
+class TestMechDriverDeleteNetworkPostCommit(NetworkingAnsibleTestCase):
+    def setUp(self):
+        super(TestMechDriverDeleteNetworkPostCommit, self).setUp()
+
+    def test_delete_network_postcommit(self, mock_delete_network):
+        self.mech.delete_network_postcommit(self.mock_net_context)
+        mock_delete_network.assert_called_once_with(self.testhost,
+                                                    self.testsegid)
+
+    def test_delete_network_postcommit_fails(self, mock_delete_network):
+        mock_delete_network.side_effect = Exception()
+        self.mech.delete_network_postcommit(self.mock_net_context)
+        mock_delete_network.assert_called_once_with(self.testhost,
+                                                    self.testsegid)
+
+    def test_delete_network_postcommit_not_vlan(self, mock_delete_network):
+        self.mock_net_context.current['provider:network_type'] = 'not-vlan'
+        self.mech.delete_network_postcommit(self.mock_net_context)
+        mock_delete_network.assert_not_called()
+
+    def test_delete_network_postcommit_not_segmentation_id(self,
+                                                           mock_delete_netwrk):
+        self.mock_net_context.current['provider:segmentation_id'] = ''
+        self.mech.delete_network_postcommit(self.mock_net_context)
+        mock_delete_netwrk.assert_not_called()
