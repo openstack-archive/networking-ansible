25ef00febcfa7f4219bedf01ae71fd9a9934eee0
Jakub Libosvar <libosvar@redhat.com>
2018-06-18 04:46:03 +0000

    hacking: Enable checks that are disabled by default
    Required hacking version was bumped in order to bring new checks. Also
    following checks are enabled in this patch:
     - [H106] Don't put vim configuration in source files.
     - [H203] Use assertIs(Not)None to check for None.
     - [H204] Use assert(Not)Equal to check for equality.
     - [H205] Use assert(Greater|Less)(Equal) for comparison.
     - [H904] Delay string interpolations at logging calls.
    
    Change-Id: I7c8c376c8127571acb6c6a8ca59099bd58d065d2

--- networking_ansible/config.py
+++ networking_ansible/config.py
@@ -52,5 +52,5 @@ def build_ansible_inventory():
             dev_cfg = {k: v[0] for k, v in hosts[host].items()}
             inventory[dev_id] = dev_cfg
 
-    LOG.info('Ansible Host List: %s' % ', '.join(inventory))
+    LOG.info('Ansible Host List: %s', ', '.join(inventory))
     return {'all': {'hosts': inventory}}
