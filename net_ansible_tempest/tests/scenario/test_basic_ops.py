d39c7d69fb4e57f2e2d1a947ecd9d7bd1f3b1e22
Jakub Libosvar <libosvar@redhat.com>
2018-06-26 12:18:28 +0000

    Add test against OVS
    The patch adds simple test where LLC information is passed to a Neutron
    logical port. This triggers Ansible mechanism driver that is configured
    to talk to local Open vSwitch database. The port on OVS side should be
    configured with VLAN tag that is taken from Neutron logical network
    segmentation ID. VLAN tag is validated using ovsdbapp querying Port OVS
    table.
    
    update_port method for openvswitch provider is added in order to be able
    to talk to OVS DB.
    
    NOTE: Current Ansible contains bug in openvswitch_db module, the PR is
          pending review at the time of pushing this patch at
          https://github.com/ansible/ansible/pull/42110
    
    This is not a real tempest test as it assumes all-in-one and checks OVS
    installed on the local machine from which tempest is run. This is
    because we're in time pressure and such test adds a big value and there
    is no better testing framework at this moment.
    
    Change-Id: Ic73065175746a10ebef1ec731f947ee1c0919e72

---
+++ net_ansible_tempest/tests/scenario/test_basic_ops.py
@@ -0,0 +1,99 @@
+# Copyright 2018 Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_config import cfg
+from tempest.lib import decorators
+from tempest.lib import exceptions
+
+from net_ansible_tempest.tests import base
+from net_ansible_tempest.tests import utils
+
+
+class TestWithOvs(base.NetAnsibleAdminBaseTest):
+
+    @classmethod
+    def skip_checks(cls):
+        super(TestWithOvs, cls).skip_checks()
+        if not cfg.CONF.service_available.netansible:
+            raise cls.skipException("networking ansible is not enabled")
+
+    def setUp(self):
+        super(TestWithOvs, self).setUp()
+        self.ovs = utils.get_idl_singleton()
+        current_tag = self.ovs.db_get(
+            'Port', self.ovs_port_name, 'tag').execute()
+        restore_command = self.ovs.db_set(
+            'Port', self.ovs_port_name, ('tag', current_tag))
+        self.addCleanup(restore_command.execute)
+
+    def cleanup_port(self, port_id):
+        """Remove Neutron port and skip NotFound exceptions."""
+        try:
+            self.admin_ports_client.delete_port(port_id)
+        except exceptions.NotFound:
+            pass
+
+    @property
+    def ovs_bridge_name(self):
+        return cfg.CONF.net_ansible_openvswitch.bridge_name
+
+    @property
+    def ovs_bridge_mac(self):
+        return self.ovs.db_get(
+            'Interface', self.ovs_bridge_name, 'mac_in_use').execute()
+
+    @property
+    def ovs_port_name(self):
+        return cfg.CONF.net_ansible_openvswitch.port_name
+
+    def get_network_segmentation_id(self, network_id):
+        return self.admin_networks_client.show_network(
+            network_id)['network']['provider:segmentation_id']
+
+    def create_port(self, network_id):
+        port = self.admin_ports_client.create_port(
+            network_id=network_id, name=self.ovs_port_name)['port']
+        self.addCleanup(self.cleanup_port, port['id'])
+
+        host = self.admin_agents_client.list_agents(
+            agent_type='Open vSwitch agent'
+        )['agents'][0]['host']
+
+        llc = [{'switch_info': 'localhost',
+                'switch_id': self.ovs_bridge_mac,
+                'port_id': self.ovs_port_name}]
+
+        update_args = {
+            'device_owner': 'baremetal:none',
+            'device_id': 'fake-instance-uuid',
+            'admin_state_up': True,
+            'binding:vnic_type': 'baremetal',
+            'binding:host_id': host,
+            'binding:profile': {
+                'local_link_information': llc
+            }
+        }
+        self.admin_ports_client.update_port(
+            port['id'],
+            **update_args
+        )
+
+    @decorators.idempotent_id('40b81fe4-1e9c-4f10-a808-c23f85aea5e2')
+    def test_update_port(self):
+        net_id = self.admin_networks_client.list_networks(
+            name='private')['networks'][0]['id']
+        self.create_port(net_id)
+        current_tag = self.ovs.db_get('Port', 'net-ans-p0', 'tag').execute()
+        network_segmentation_id = self.get_network_segmentation_id(net_id)
+        self.assertEqual(network_segmentation_id, current_tag)
