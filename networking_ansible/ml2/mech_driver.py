6995873ddc801a296342273e5296175245ce2da4
Jakub Libosvar <libosvar@redhat.com>
2019-02-08 13:12:12 +0100

    DNM: Test Queens jobs
    Change-Id: I41179757632a8946c294956e7601e049bdb189f5

--- networking_ansible/ml2/mech_driver.py
+++ networking_ansible/ml2/mech_driver.py
@@ -12,6 +12,7 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+#
 
 from neutron.db import provisioning_blocks
 from neutron.plugins.ml2.common import exceptions as ml2_exc
